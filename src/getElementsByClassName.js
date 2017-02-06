cabd542adc08ed7908e06e119b17db2ead48662e
Paul Mills <paulmills@tests-MacBook-Pro-2.local>
2017-02-06 11:43:50 -0800

    Write out sudo code to outline program

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,14 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
+var getElementsByClassName = function(className) {
   // your code here
+  // set up a results array;
+  // define a new function searchNodes
+    // check if the node passed into the function matches the given classname
+    // iterate through the child nodes of the node
+    // run child node through search nodes
+  //initialize the funcion by passing document.body
+  //return the results array
+
 };
