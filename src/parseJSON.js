5ac6fbfa0d060bf559de54471747b1ea1a6073b8
Paul Mills <paulmills@tests-MacBook-Pro-2.local>
2017-02-06 21:55:15 -0800

    Passing first four tests,
    Added JSON parsing for objects, null, true, false,
    strings and empty strings

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -2,20 +2,41 @@
 // var parseJSON = JSON.parse;
 
 // but you're not, so you'll write it from scratch:
-var parseJSON = function(json, type) {
+var parseJSON = function(json) {
   // result should equal an array,  object or nothing depending on what json[0] is
   var result;
-  if (type === undefined){
-    if (json[0] === '['){
-      result = [];
-    } else if (json[0] === '{'){
-      result = {};
+  if (json[0] === '[') {
+    result = [];
+  }
+  if (json[0] === '{') {
+    result = {};
+    var objArray = json.slice(1, json.length - 1).split(':');
+    for (var i = 0; i< objArray.length-1; i++){
+      var key = parseJSON(objArray[i]);
+      var value = parseJSON(objArray[i + 1].slice(1, objArray[i + 1].length));
+      result[key] = value;
     }
+    return result;
+  }
+  if (json === "\"\"") {
+    return "";
+  }
+  if (json[0] === '"' && json !== '\"\"') {
+    return json.slice(1,json.length-1);
+  }
+  // if json === 'null', return null
+  if (json === 'null') {
+    return null;
+  }
+  // if json === 'true', return true
+  if (json === 'true') {
+    return true;
+  }
+  // if json === 'false', return false
+  if (json === 'false') {
+    return false;
   }
   // check if Number(json) !== NaN, in which case return Number(string),
-  // if json === 'null', return null
-  // if json === 'true', return true
-  // if json === 'false', return false
   // if json[0] === '"', return a string with out the ""
   // if json is obj, .split(':')
     // count length of array to deterimine number of pairs
@@ -23,6 +44,5 @@ var parseJSON = function(json, type) {
     // join indexes left of index and right of index
     //  run throuhn parseJSON
   // if json is an array, push parseJSON(json.slice(1,json.length-2));
-
   return result;
 };
